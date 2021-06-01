221a7ca9139fc70275f0c19457470937212c2536
SMITAL CHRISTIAN <spchristian44@gmail.com>
2021-06-01 10:52:52 -0400

    Add files via upload

--- public/my-module.js
+++ public/my-module.js
@@ -1,8 +1,6 @@
-const theShire = new HobbitSettlement('The Shire', 1601, ['Marcho', 'Blanco']);
-
 const statement = () => `${theShire.name} was founded ${buckland.founding - theShire.founding} years before ${buckland.name}.`;
 
-
-import{theShire , buckland} from './another-module.js';
 export{theShire,statement};
+import{theShire , buckland} from './another-module.js';
+
 
