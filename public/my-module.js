a1cb22b1cf18ba1ab53c8f098015ee1476dc3e4d
SMITAL CHRISTIAN <spchristian44@gmail.com>
2021-06-01 10:57:18 -0400

    Add files via upload

--- public/my-module.js
+++ public/my-module.js
@@ -1,6 +1,6 @@
 const statement = () => `${theShire.name} was founded ${buckland.founding - theShire.founding} years before ${buckland.name}.`;
+import{ theShire , buckland } from './another-module.js';
+export{ theShire , statement };
 
-export{theShire,statement};
-import{theShire , buckland} from './another-module.js';
 
 
